45e80e7e0a527112889a30af9b926bbdb727c6b5
Geoff Wagstaff <geoff@gosquared.com>
2021-09-02 13:16:52 +0000

    add source.start to simple example

--- examples/simple.ts
+++ examples/simple.ts
@@ -1,9 +1,12 @@
 import { pipeline } from 'stream';
+import Reader from '../src/reader';
 
 const { FROM_ES, TO_ES } = process.env;
 
 const source = new Reader(FROM_ES);
 const destination = new Writer(TO_ES);
+
+source.start(['index-*']);
 
 async function main() {
   await pipeline(source, destination);
