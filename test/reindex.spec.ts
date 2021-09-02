607676b5011b1fc967139d4ef0aae7a6fe7363b9
Geoff Wagstaff <geoff@gosquared.com>
2021-09-02 16:43:23 +0000

    create 2.3 client

--- test/reindex.spec.ts
+++ test/reindex.spec.ts
@@ -1,7 +1,11 @@
+import { Client } from 'elasticsearch';
+
 describe('reindex', () => {
+  const client = new Client({ host: 'http://localhost:9200' });
   const createDocument = async () => {
     await client.index({
       index: 'test',
+      type: 'test',
       id: '1',
       body: {
         title: 'foo',
