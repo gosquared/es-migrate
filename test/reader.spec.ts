b1811a10337effc0f6e93deaf9d46fc2bdee06a7
Geoff Wagstaff <geoff@gosquared.com>
2021-09-03 09:57:05 +0000

    skip incomplete test

--- test/reader.spec.ts
+++ test/reader.spec.ts
@@ -1,7 +1,7 @@
 import { Client } from '@elastic/elasticsearch';
 import Reader from '../src/reader';
 
-describe('reader', () => {
+describe.skip('reader', () => {
   it('starts scroll', () => {
     const client = new Client({ node: 'http://localhost:9200' });
     const reader = new Reader(client);
