78786c1c8579e5c69e0c745946c8bb3ed43e45e4
Geoff Wagstaff <geoff@gosquared.com>
2021-09-06 11:07:24 +0000

    add reindex test

--- test/reindex.spec.ts
+++ test/reindex.spec.ts
@@ -1,7 +1,13 @@
 import { Client } from 'elasticsearch';
+import { Client as Client2 } from '@elastic/elasticsearch';
+import { Reindex } from '@elastic/elasticsearch/api/requestParams';
+
+const client = new Client({ host: 'http://localhost:9250' });
+const client2 = new Client2({ node: 'http://localhost:9245' });
+
+const refresh = () => client.indices.refresh({ index: '_all' });
 
 describe('reindex', () => {
-  const client = new Client({ host: 'http://localhost:9250' });
   const createDocument = async () => {
     await client.index({
       index: 'test',
@@ -14,7 +20,23 @@ describe('reindex', () => {
     });
   };
 
+  // https://www.elastic.co/guide/en/elasticsearch/reference/7.10/docs-reindex.html#reindex-from-remote
+  const reindexDocument = async () => {
+    const host = 'http://localhost:9250';
+    const remote = { host };
+    const index = 'test';
+    const query = { match_all: {} };
+    const source = { remote, index, query };
+    const dest = { index };
+    const body = { source, dest };
+    const params: Reindex = { body };
+    const result = await client2.reindex(params);
+    console.log(JSON.stringify(result));
+  };
+
   before(createDocument);
+  before(refresh);
+  before(reindexDocument);
 
   it('reindexes document', () => {
 
