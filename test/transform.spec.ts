7b04a1717ae44a115ea73e2abf67756de43b7cfe
Geoff Wagstaff <geoff@gosquared.com>
2021-09-07 09:14:28 +0000

    add transform test

---
+++ test/transform.spec.ts
@@ -0,0 +1,45 @@
+import { Client } from 'elasticsearch';
+import { Client as Client2 } from '@elastic/elasticsearch';
+import { Reindex } from '@elastic/elasticsearch/api/requestParams';
+
+const client = new Client({ host: 'http://localhost:9250' });
+const client2 = new Client2({ node: 'http://localhost:9245' });
+
+const refresh = () => client.indices.refresh({ index: '_all' });
+
+const createDocument = async () => {
+  await client.index({
+    index: 'test',
+    type: 'test',
+    id: '1',
+    body: {
+      title: 'foo',
+      description: 'bar',
+    },
+  });
+};
+
+const reindexDocument = async () => {
+  const host = 'http://es-migrate-es1:9200';
+  const remote = { host };
+  const index = 'test';
+  const query = { match_all: {} };
+  const source = { remote, index, query };
+  const dest = { index };
+  const src = 'ctx._source.ty = ctx._type';
+  const script = { source: src, lang: 'painless' };
+  const body = { source, dest, script };
+  const params: Reindex = { body };
+  const result = await client2.reindex(params);
+  console.log(JSON.stringify(result));
+};
+
+describe('transform', () => {
+  before(createDocument);
+  before(refresh);
+  before(reindexDocument);
+
+  it('adds type field', () => {
+
+  });
+});
